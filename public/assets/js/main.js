a21438b0a42df61a2eac59c2d519931acc60091a
mayank1920 <mayankdobriyal1920@gmail.com>
2023-06-22 11:22:35 +0530

    New changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -61,6 +61,7 @@
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
+    console.log('mobile-nav-toggle');
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
